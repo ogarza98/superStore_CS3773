015d87cdc4b09283d8bc52f772b2fdda373af3c7
Oscar Garza <ogarza98@live.com>
2019-12-06 16:23:39 -0600

    oscar push

--- django_project/superStore_website/views.py
+++ django_project/superStore_website/views.py
@@ -1,5 +1,9 @@
 from .models import Good
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
 
 
 def home(request):
@@ -8,3 +12,4 @@ def home(request):
     }
 
     return render(request, 'superStore_website/home.html', context)
+
