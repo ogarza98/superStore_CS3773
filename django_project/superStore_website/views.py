988bd07cc27d1d76bb934ebf49d3207450474238
Oscar Garza <ogarza98@live.com>
2019-12-07 16:09:27 -0600

    merge conflict fix

--- django_project/superStore_website/views.py
+++ django_project/superStore_website/views.py
@@ -6,11 +6,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 
-
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 from django.db.models import Q
 
 
