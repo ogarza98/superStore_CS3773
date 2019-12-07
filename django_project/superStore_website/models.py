dca6ea2657c65499ba4423caf47e1f06efee4147
alberto-guillen <vcb781@my.utsa.edu>
2019-12-06 22:26:33 -0600

    updated models.py

--- django_project/superStore_website/models.py
+++ django_project/superStore_website/models.py
@@ -17,3 +17,34 @@ class Good(models.Model):
 
     def __str__(self):
         return self.item_name
+
+class Cart(models.Model):
+    STATES = (
+        ('AL','AL'), ('AK','AK'), ('AZ','AZ'), ('AR','AR'), ('CA','CA'), ('CO','CO'), ('CT','CT'), 
+        ('DE','DE'), ('FL','FL'), ('GA','GA'), ('HI','HI'), ('ID','ID'), ('IL','IL'), ('IN''IN',),
+        ('IA','IA'), ('KS','KS'), ('KY','KY'), ('LA','LA'), ('ME','ME'), ('MD','MD'), ('MA','MA'), 
+        ('MI','MI'), ('MN','MN'), ('MS','MS'), ('MO','MO'), ('MT','MT'), ('NE''NE',), ('NV','NV'), 
+        ('NH','NH'), ('NJ','NJ'), ('NM','NM'), ('NY','NY'), ('NC','NC'), ('ND','ND'), ('OH','OH'),
+        ('OK','OK'), ('OR','OR'), ('PA','PA'), ('RI','RI'), ('SC','SC'), ('SD','SD'), ('TN','TN'),
+        ('TX','TX'), ('UT','UT'), ('VT','VT'), ('VA','VA'), ('WA','WA'), ('WV','WV'), ('WI','WI'),
+        ('WY','WY'),
+    )
+
+    PAYMENT_TYPE = (
+        ('Credit Card', 'Credit Card'),
+        ('Debit Card', 'Debit Card'),
+    )
+
+    first_name = models.CharField(max_length=80)
+    last_name  = models.CharField(max_length=80)
+    user_name  = models.CharField(max_length=80)
+    email      = models.CharField(max_length=80)
+    street     = models.CharField(max_length=100)
+    country    = models.CharField(max_length=20, choices='United States', default='Choose')
+    state      = models.CharField(max_length=2, choices=STATES, default='State')
+    zipCode    = models.IntegerField()
+    nameOnCd   = models.CharField(max_length=160)
+    cardNum    = models.IntegerField()
+    expireDt   = models.IntegerField()
+    cvv        = models.CharField(max_length=5)
+
