774201e35e173e75dc6424e58138ff7741886f7b
alberto-guillen <vcb781@my.utsa.edu>
2019-12-06 22:35:01 -0600

    models.py updated

--- django_project/superStore_website/models.py
+++ django_project/superStore_website/models.py
@@ -21,13 +21,17 @@ class Good(models.Model):
 class Cart(models.Model):
     STATES = (
         ('AL','AL'), ('AK','AK'), ('AZ','AZ'), ('AR','AR'), ('CA','CA'), ('CO','CO'), ('CT','CT'), 
-        ('DE','DE'), ('FL','FL'), ('GA','GA'), ('HI','HI'), ('ID','ID'), ('IL','IL'), ('IN''IN',),
+        ('DE','DE'), ('FL','FL'), ('GA','GA'), ('HI','HI'), ('ID','ID'), ('IL','IL'), ('IN','IN'),
         ('IA','IA'), ('KS','KS'), ('KY','KY'), ('LA','LA'), ('ME','ME'), ('MD','MD'), ('MA','MA'), 
-        ('MI','MI'), ('MN','MN'), ('MS','MS'), ('MO','MO'), ('MT','MT'), ('NE''NE',), ('NV','NV'), 
+        ('MI','MI'), ('MN','MN'), ('MS','MS'), ('MO','MO'), ('MT','MT'), ('NE','NE'), ('NV','NV'), 
         ('NH','NH'), ('NJ','NJ'), ('NM','NM'), ('NY','NY'), ('NC','NC'), ('ND','ND'), ('OH','OH'),
         ('OK','OK'), ('OR','OR'), ('PA','PA'), ('RI','RI'), ('SC','SC'), ('SD','SD'), ('TN','TN'),
         ('TX','TX'), ('UT','UT'), ('VT','VT'), ('VA','VA'), ('WA','WA'), ('WV','WV'), ('WI','WI'),
         ('WY','WY'),
+    )
+
+    COUNTRY = (
+        ('United States','United States'),
     )
 
     PAYMENT_TYPE = (
@@ -40,7 +44,7 @@ class Cart(models.Model):
     user_name  = models.CharField(max_length=80)
     email      = models.CharField(max_length=80)
     street     = models.CharField(max_length=100)
-    country    = models.CharField(max_length=20, choices='United States', default='Choose')
+    country    = models.CharField(max_length=20, choices=COUNTRY, default='Choose')
     state      = models.CharField(max_length=2, choices=STATES, default='State')
     zipCode    = models.IntegerField()
     nameOnCd   = models.CharField(max_length=160)
